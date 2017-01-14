9ad44ed6446a0c347cfb56c7157ba8f03bcc1c7c
fabiodl <fabiodl@gmail.com>
2017-01-15 02:33:05 +0900

    modified help

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 int main(int argc,char** argv){
   
   if (argc<static_cast<int>(MegaNkro::NUMKEYS+1)){
-    std::cout<<"Usage "<<argv[0]<<" followed by "<<MegaNkro::NUMKEYS<<" hex values, according to Universal Serial Bus HID Usage Tables, Table 12: Keyboard/Keypad Page "<<std::endl;
+    std::cout<<"Usage "<<argv[0]<<" followed by "<<MegaNkro::NUMKEYS<<" hex values, according to \"Universal Serial Bus HID Usage Tables\", Table 12: Keyboard/Keypad Page "<<std::endl;
+    std::cout<<"key order is"<<"pad1 up down left right b c a start"<<std::endl<<"pad2 up down left right b c a start"<<std::endl<<"pad1 z y x mode"<<std::endl<<"pad2 z y x mode"<<std::endl;
+      
     return 0;
   }
   
