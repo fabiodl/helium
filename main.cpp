a37994f4af0533720298f55b2233bd614ba86fb0
Fabio Dalla Libera <fabiodl@gmail.com>
2017-01-25 11:56:40 +0900

    file parsing added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <map>
 #include <helium/os/usbHid.h>
+#include <fstream>
 #include <stdlib.h>
 #include "megankro.h"
 
-static const char* keys[256]={
+static const int KEYBOARDKEYS=256;
+static const char* keyboardKeys[KEYBOARDKEYS]={
   /*00*/ "noevent", "rollover","postfail","undefinedError",  
   /*04*/"a","b","c","d",                                    
   /*08*/"e","f","g","h",                                    
@@ -14,7 +16,7 @@ static const char* keys[256]={
   /*1C*/"y","z","1","2",
   /*20*/"3","4","5","6",  
   /*24*/"7","8","9","0",
-  /*28*/"enter","escape","delete","tab",
+  /*28*/"enter","escape","backspace","tab",
   /*2C*/"space","minus","equal","openbracket",
   /*30*/"closebracket","backslash","nonus_sharp","semicolon",
   /*34*/"singlequote","backquote","comma","period",
@@ -37,14 +39,14 @@ static const char* keys[256]={
   /*78*/"stop","again","undo","cut",  
   /*7C*/"copy","paste","find","mute",
   /*80*/"volumeup","volumedown","lockingcapslock","lockingnumlock",  
-  /*84*/"lockingscrolllock","keypad_comma","keypad_equal","international1",
+  /*84*/"lockingscrolllock","keypad_comma","keypad_equal_as400","international1",
   /*88*/"international2","international3","international4","international5",
   /*8C*/"international6","international7","international8","international9",
   /*90*/"lang1","lang2","lang3","lang4",
   /*94*/"lang5","lang6","lang7","lang8",
   /*98*/"lang9","erase","sysreq","cancel",  
   /*9C*/"clear","prior","return","separator",
-  /*A0*/"out","oper","again","crsel",  
+  /*A0*/"out","oper","clearagain","crsel",  
   /*A4*/"exsel","#a5","#a6","#a7",
   /*A8*/"#a8","#a9","#aa","#ab",
   /*AC*/"#ac","#ad","#ae","#af",
@@ -71,6 +73,38 @@ static const char* keys[256]={
 };
 
 
+static const int PADS=2;
+static const int PADBUTTONS=12;
+static const char* padKeys[PADBUTTONS]={
+"up","down","left","right",
+  "a","b","c","start",
+  "x","y","z","mode"
+};
+
+
+
+
+static const int btnDestination[PADS][PADBUTTONS]={
+
+  {
+  0,1,2,3,
+  6,4,5,7,
+  18,17,16,19
+  },
+
+  {
+  8,9,10,11,
+  14,12,13,15,
+  22,21,20,23
+    }
+
+}  ;
+
+
+
+
+
+
 bool equals(const char* s1,const char* s2){
   for (;*s1!=0&&*s2!=0;s1++,s2++){
     while(*s1=='_'){s1++;}
@@ -82,33 +116,110 @@ bool equals(const char* s1,const char* s2){
   return !(*s1||*s2);
 }
 
+int getKey(const std::string& s,const char** keys,const std::string& dictName){
+  for (int i=0;i<256;i++){
+    if (equals(keys[i],s.c_str())){
+      return i;
+    }
+  }
+  throw helium::exc::InvalidDataValue(dictName,s);
+}
+
+
+void sanityCheck(){
+  for (int i=0;i<256;i++){
+      for (int j=0;j<256;j++){
+        if (i==j) continue;
+        if (equals(keyboardKeys[i],keyboardKeys[j])){
+          std::cout<<"key internal match!!!"<<keyboardKeys[i]<<" "<<keyboardKeys[j]<<std::endl;
+        }
+      }
+  }
+
+}
+
+
+void getKeySequence(uint8_t* outs,const int* destination,const char* fname){
+  std::ifstream f(fname);
+  std::map<int,int> mapping;
+  while(!f.eof()){
+    std::string pad,keyboard;
+    f>>pad>>keyboard;
+    if (!f) break;
+    int padKey=getKey(pad,padKeys,"pad key");
+    int keyboardKey=getKey(keyboard,keyboardKeys,"keyboard key");
+    if (mapping.find(padKey)!=mapping.end()){
+      throw helium::exc::DuplicateID(pad+" button assigned to multiple keys in file "+fname);
+    }
+    mapping[padKey]=keyboardKey;
+  }
+  for (int i=0;i<PADBUTTONS;i++){
+    if (mapping.find(i)==mapping.end()){
+      throw helium::exc::MissingData(std::string(padKeys[i])+" button in file "+fname);
+    }
+    outs[destination[i]]=mapping[i];
+  }  
+}
+
+
+std::string describeButton(int btnId){
+
+  for (int p=0;p<PADS;p++){
+    for (int b=0;b<PADBUTTONS;b++){
+      if (btnDestination[p][b]==btnId){
+        std::stringstream ss;
+        ss<<"pad "<<(p+1)<<" button "<<padKeys[b];
+        return ss.str();
+      }
+    }
+  }
+  return "";//should not reach here
+}
+
+void checkDuplicates(uint8_t* padbuttons){
+  for (int i=0;i<2*PADBUTTONS;i++){
+    for (int j=0;j<2*PADBUTTONS;j++){
+      if (i==j) continue;
+      if (padbuttons[i]==padbuttons[j]){
+        throw helium::exc::DuplicateID(std::string("key ")+keyboardKeys[padbuttons[i]]+" associated to both "+
+                                       describeButton(i)+" and "+describeButton(j));
+                                       
+      }
+
+    }
+  }
+}
+
 
 int main(int argc,char** argv){
 
-  for (int i=0;i<256;i++){
-    //std::cout<<i<<":"<<keys[i]<<std::endl;
-    if (equals(keys[i],argv[1])){
-      std::cout<<keys[i]<<std::endl;
-    }
-  }
 
   
-  
-  if (argc<static_cast<int>(MegaNkro::NUMKEYS+1)){
-    std::cout<<"Usage "<<argv[0]<<" followed by "<<MegaNkro::NUMKEYS<<" hex values, according to \"Universal Serial Bus HID Usage Tables\", Table 12: Keyboard/Keypad Page "<<std::endl;
-    std::cout<<"key order is"<<"pad1 up down left right b c a start"<<std::endl<<"pad2 up down left right b c a start"<<std::endl<<"pad1 z y x mode"<<std::endl<<"pad2 z y x mode"<<std::endl;
-      
+  if (argc<PADS+1){
+    std::cout<<"Usage "<<argv[0]<<" followed by two filenames."<<std::endl<<
+      "each filename has 12 lines, with the format <pad button><space><keyboard key>"<<std::endl<<
+      "AVAILABLE PAD BUTTONS"<<std::endl;
+    
+    for (int i=0;i<PADBUTTONS;i++){
+      std::cout<<" "<<padKeys[i]<<std::endl;
+    }
+    std::cout<<"AVAILABLE KEYBOARD KEYS"<<std::endl;
+    for (int i=0;i<KEYBOARDKEYS;i++){
+      char buffer[3];
+      sprintf(buffer,"%02x ",i);
+      std::cout<<" "<<buffer<<keyboardKeys[i]<<std::endl;
+    }      
     return 0;
   }
 
 
-  
- 
-  uint8_t keys[MegaNkro::NUMKEYS];
-  for (size_t i=0;i<MegaNkro::NUMKEYS;i++){
-    keys[i]=static_cast<uint8_t>(strtol(argv[i+1],NULL,16));
-    std::cout<<"key "<<(int)keys[i]<<std::endl;
+  uint8_t keys[PADS*PADBUTTONS];
+  for (int i=0;i<PADS;i++){
+    getKeySequence(keys,btnDestination[i],argv[i+1]);
   }
+  checkDuplicates(keys);
+
+ 
   MegaNkro nkro;
   nkro.set(keys);
 
